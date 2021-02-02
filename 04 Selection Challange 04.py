c5fcfe1a34b812d8b41dffe6caf9111b453291d4
DavidAntociOPHS <71763407+DavidAntociOPHS@users.noreply.github.com>
2021-02-02 03:30:34 -0800

    Delete 04 Selection Challange 04.py

--- 04 Selection Challange 04.py
+++
@@ -1,6 +0,0 @@
-colo = input("What is you favourite colour--->")
-
-if colo == "red" and "RED" and "Red":
-    print("I like red too")
-else:
-    print("I dont like that" ,colo, "it is for losers")
